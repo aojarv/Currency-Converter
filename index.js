ce73af2212aa922721ac55f2f532a3e284d7ef22
Antti Järveläinen <51124276+aojarv@users.noreply.github.com>
2019-11-03 16:31:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import { TextField, FormControl, InputLabel, MenuItem, Select } from '@material-ui/core'
 
-import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+const App = () => {
+  const [currency1, setCurrency1] = useState('')
+  const [currency2, setCurrency2] = useState('')
 
-class App extends React.Component{
+  const handleChange1 = event => {
+    setCurrency1(event.target.value);
+  };
 
-  constructor(props){
-    super(props);
-    this.state = {
+  const handleChange2 = event => {
+    setCurrency2(event.target.value);
+  };
 
-    }
-  }
-
-  handleChange = (event) => {
-    setCurrency(event.target.value);
-  }
-
-render(){
   return(
     <div>
-      <FormControl>
-        <InputLabel id="demo-simple-select-label">Currency</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={currency}
-          onChange={handleChange}
-        >
-          <MenuItem value={EUR}>EUR</MenuItem>
-          <MenuItem value={USD}>USD</MenuItem>
-          <MenuItem value={SEK}>SEK</MenuItem>
-        </Select>
-      </FormControl>
+      <p>
+        <FormControl>
+          <InputLabel id="currency1">From</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={currency1}
+            onChange={handleChange1}
+          >
+            <MenuItem value={"EUR"}>EUR</MenuItem>
+            <MenuItem value={"USD"}>USD</MenuItem>
+            <MenuItem value={"SEK"}>SEK</MenuItem>
+          </Select>
+        </FormControl>
+      </p>
+
+      <p>
+        <FormControl>
+          <InputLabel id="currency2">To what</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={currency2}
+            onChange={handleChange2}
+          >
+            <MenuItem value={"USD"}>USD</MenuItem>
+            <MenuItem value={"EUR"}>EUR</MenuItem>
+            <MenuItem value={"SEK"}>SEK</MenuItem>
+          </Select>
+        </FormControl>
+      </p>
     </div>
-  );
+  )
 }
-
-}
-
-
 
 ReactDOM.render(
-  <App />,
+  <App/>,
   document.getElementById('root')
-);
+)
